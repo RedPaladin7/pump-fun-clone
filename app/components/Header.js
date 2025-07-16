b0d94a9dd4b035eadc904307fb3e6ca5d5a60517
RedPaladin7 <ishans7325@gmail.com>
2025-07-16 22:51:50 +0200

    completed token, trade, list and header component

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,23 @@
 import { ethers } from "ethers"
 
 function Header({ account, setAccount }) {
+  async function connectHandler() {
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+    // just returns strings of addresses
+    const account = ethers.getAddress(accounts[0])
+    // confirms the address is valid
+    setAccount(account)
+  }
 
   return (
     <header>
+      <p className="brand">fun.pump</p>
 
+      {account ? (
+        <button onClick={connectHandler} className="btn--fancy">[{account.slice(0, 6) + '...' + account.slice(28, 42)}]</button>
+      ) : (
+        <button onClick={connectHandler} className="btn--fancy">[ connect ]</button>
+      )}
     </header>
   );
 }

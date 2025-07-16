9513ced547aaa38c366d857b5eb482303b72e6e6
RedPaladin7 <ishans7325@gmail.com>
2025-07-16 12:01:48 +0200

    completed and test buying functions

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.27;
 import {Token} from "./Token.sol";
 
 contract Factory {
+    uint256 public constant TARGET = 3 ether;
+    uint256 public constant TOKEN_LIMIT = 500000 ether;
     uint256 public immutable fee;
     address public owner;
 
@@ -21,6 +23,7 @@ contract Factory {
     }
 
     event Created(address indexed token);
+    event Buy(address indexed token, uint256 amount);
 
     constructor(uint256 _fee){
         fee = _fee;
@@ -29,6 +32,17 @@ contract Factory {
 
     function getTokenSale(uint256 _index) public view returns(TokenSale memory){
         return tokenToSale[tokens[_index]];
+    }
+
+    function getCost(uint256 _sold) public pure returns (uint256){
+        uint256 floor = 0.0001 ether;
+        uint256 step = 0.0001 ether;
+        uint256 increment = 10000 ether;
+        // the price will increase by 0.0001 ether for every 10000 tokens bought
+        // the base price will be 0.0001 ether
+
+        uint256 cost = (step * (_sold / increment)) + floor;
+        return cost;
     }
 
     function create(string memory _name, string memory _symbol) external payable{
@@ -49,5 +63,27 @@ contract Factory {
 
         tokenToSale[address(token)] = sale;
         emit Created(address(token));
+    }
+
+    function buy(address _token, uint256 _amount) external payable {
+        TokenSale storage sale = tokenToSale[_token];
+
+        require(sale.isOpen == true, "Factory: Buying closed");
+        require(_amount >= 1 ether, "Factory: Amount too low");
+        require(_amount <= 10000 ether, "Factory: Amount exceeded");
+
+        uint256 cost = getCost(sale.sold);
+        uint256 price = cost * (_amount / 10 ** 18);
+
+        require(msg.value >= price, "Factory: Insufficient ETH received");
+
+        sale.sold += _amount;
+        sale.raised += price;
+
+        if(sale.sold >= TOKEN_LIMIT || sale.raised >= TARGET){
+            sale.isOpen = false;
+        }
+        Token(_token).transfer(msg.sender, _amount);
+        emit Buy(_token, _amount);
     }
 }

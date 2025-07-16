2e751b86eb33f90feeffcbd63b1c3d192f3500f7
RedPaladin7 <ishans7325@gmail.com>
2025-07-16 11:36:46 +0200

    completed and tested token creation functions

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Factory {}
+import {Token} from "./Token.sol";
+
+contract Factory {
+    uint256 public immutable fee;
+    address public owner;
+
+    address[] public tokens;
+    uint256 public totalTokens;
+    mapping(address=>TokenSale) public tokenToSale;
+
+    struct TokenSale {
+        address token;
+        string name;
+        address creator;
+        uint256 sold;
+        uint256 raised;
+        bool isOpen;
+    }
+
+    event Created(address indexed token);
+
+    constructor(uint256 _fee){
+        fee = _fee;
+        owner = msg.sender;
+    }
+
+    function getTokenSale(uint256 _index) public view returns(TokenSale memory){
+        return tokenToSale[tokens[_index]];
+    }
+
+    function create(string memory _name, string memory _symbol) external payable{
+        require(msg.value >= fee, "Factory: Creator fee not met");
+
+        Token token = new Token(msg.sender, _name, _symbol, 1000000 ether);
+        tokens.push(address(token));
+        totalTokens++;
+
+        TokenSale memory sale = TokenSale(
+            address(token),
+            _name, 
+            msg.sender,
+            0, 
+            0, 
+            true
+        );
+
+        tokenToSale[address(token)] = sale;
+        emit Created(address(token));
+    }
+}
